1b561792b3ac060d13053711fe9fa6862bf3f2a5
CalvinTyminski <calvintyminski@hotmail.com>
2026-02-06 20:29:10 -0700

    Added class Exercise and modified class Workout

---
+++ WorkoutTracker/Models/Exercise.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutTracker.Models
+{
+    public class Exercise 
+    { 
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Sets { get; set; }
+
+        [Required]
+        public int Reps { get; set; }
+
+        [Required]
+        public double weight { get; set; }
+
+        public int WorkoutId { get; set; } 
+
+        public Workout? Workout { get; set; }
+
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
